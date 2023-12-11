b884e238a866a0ce48914798ad2b3e15f6b138c4
macs0416 <anmol.s@sigdesk.com>
2023-12-11 18:01:39 +0530

    meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from 'next/image'
 import HomePage from './Components/Home/HomePage'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Anmol Singh',
+  description: 'Experienced software engineer with a track record of delivering innovative solutions, ensuring high-quality software, and driving project success. Skilled in analyzing complex requirements, architecting robust systems, and optimizing performance with efficient algorithms and data structures. Proficient in multiple programming languages and frameworks, emphasizing clean, maintainable code. Collaborative team player experienced in meeting business objectives through cross-functional collaboration.',
+}
 
 export default function Home() {
   return (
